d73f4298b322453d3af571d38c1c06c2d90ac352
Lucas <lucasmacielaguiar@gmail.com>
2020-06-11 16:58:54 -0300

    Add Aws credentials in env

--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -4,7 +4,9 @@ import AWS from 'aws-sdk';
 import crypto from 'crypto';
 
 AWS.config.update({
-  region: 'us-east-1'
+  region: 'us-east-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
 const S3 = new AWS.S3();
